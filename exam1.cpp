74df1da27e42da6accea081e86ccd854636d8f5f
Unknown <sassytofu@gmail.com>
2018-02-20 12:28:56 +0700

    ข้อ1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-
-        return 0;
+    float a,b,cmd,o_1,o_2,o_3,o_4;
+    cout<< "Welcome to CLI Calculator V1.00"<<endl;
+    cout<<"Number 1 : ";
+    cin>>a;
+    cout<<"Number 2 : ";
+    cin>>b;
+    do{
+        cout<<"MENU\n1.+\n2.-\n3.*\n4./\n5.quit"<<endl;
+        cin >>cmd;
+        if(cmd==1) {
+            o_1=a+b;
+            cout<<"Result = "<<o_1<<endl;
+        }else if(cmd==2) {
+            o_2=a-b;
+            cout<<"Result = "<<o_2<<endl;
+        }else if(cmd==3) {
+            o_3=a*b;
+            cout<<"Result = "<<o_3<<endl;
+        }else if(cmd==4) {
+            o_4=a/b;
+            cout<<"Result = "<<o_4<<endl;
+        }{break;}
+    }while(0);
+ return 0;
 }
